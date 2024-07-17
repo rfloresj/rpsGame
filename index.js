0c586f80e7ccdffefbbe398865890dc4b65fef63
rflores <104962825+rfloresj@users.noreply.github.com>
2024-07-17 17:49:02 -0600

    index:modularize-game-components

---
+++ index.js
@@ -0,0 +1,19 @@
+const readline = require('readline')
+const { validateArgs } = require('./utils/validateArgs')
+const { Game } = require('./models/rpsGame')
+const { HelpTable } = require('./models/HelpTable')
+const { promptUser } = require('./controllers/gameController.js')
+
+const args = process.argv.slice(2);
+validateArgs(args)
+
+const game = new Game(args)
+console.log(`HMAC: ${game.hmac}`);
+game.showMoves();
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+promptUser(rl, game, args);
