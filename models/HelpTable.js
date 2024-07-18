1e9a697ce085155b1c41256d5d329c3d7dde3f48
rflores <104962825+rfloresj@users.noreply.github.com>
2024-07-17 20:27:40 -0600

    HelpTable:generate-help-table

---
+++ models/HelpTable.js
@@ -0,0 +1,35 @@
+const { Table } = require('console-table-printer');
+
+class HelpTable {
+  constructor(moves) {
+    this.moves = moves;
+  }
+
+  generateTable() {
+    const table = new Table();
+    const header = ['PC\\User >', ...this.moves];
+    table.addRow(header, { color: 'cyan' });
+
+    this.moves.forEach((move, index) => {
+      const row = [move];
+      this.moves.forEach((_, innerIndex) => {
+        row.push(this.getResult(index, innerIndex));
+      });
+      table.addRow(row);
+    });
+    table.printTable;
+  }
+
+  getResult(pcIndex, userIndex){
+    const half = Math.floor(this.moves.length / 2);
+    if (pcIndex === userIndex){
+        return 'Draw';
+    } else if ((pcIndex - userIndex + this.moves.length) % this.moves.length <= half){
+        return 'Win';
+    } else {
+        return 'Lose';
+    }
+  }
+}
+
+module.exports = { HelpTable }
